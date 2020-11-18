9e5887516e17dac05b3745633d55c884cca2bfd6
hunghaubmt <voduchau01011997@gmail.com>
2020-11-18 22:16:37 +0700

    set border table user + icon location

--- src/modules/containers/Main/SideBar.js
+++ src/modules/containers/Main/SideBar.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { Layout, Menu, Switch } from 'antd';
 import {
-    MenuUnfoldOutlined,
+    HomeOutlined,
     BulbOutlined,
     UserOutlined,
     VideoCameraOutlined,
     UploadOutlined,
+    EnvironmentOutlined
 } from '@ant-design/icons';
 import { Link } from "react-router-dom";
 import logo from '../../../assets/images/logo.png';
+import { useLocation } from "react-router-dom";
 
 const { Header, Sider, Content } = Layout;
 
 const SideBar = (props) => {
+    let location = useLocation();
     const {
         theme,
         collapsed,
@@ -20,6 +23,8 @@ const SideBar = (props) => {
         setCollapsed,
         matches
     } = props;
+
+    console.log(location.pathname, 'props')
 
     return (
         <Sider
@@ -40,24 +45,24 @@ const SideBar = (props) => {
             <div className="logo-sidebar">
                 <img src={logo} alt="logo-sider" />
             </div>
-            <Menu className="ant-menu-sider" theme={theme} mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
+            <Menu className="ant-menu-sider" theme={theme} mode="inline" defaultSelectedKeys={[location.pathname]}>
+                <Menu.Item key="/home" icon={<HomeOutlined />}>
                     <Link to="/home">
                         Home
                     </Link>
                 </Menu.Item>
-                <Menu.Item key="2" icon={<UserOutlined />}>
+                <Menu.Item key="/user" icon={<UserOutlined />}>
                     <Link to="/user">
                         User
                     </Link>
                 </Menu.Item>
-                <Menu.Item key="3" icon={<VideoCameraOutlined />}>
+                <Menu.Item key="/player" icon={<VideoCameraOutlined />}>
                     Player
                 </Menu.Item>
-                <Menu.Item key="4" icon={<UploadOutlined />}>
+                <Menu.Item key="/upload" icon={<UploadOutlined />}>
                     Upload
                 </Menu.Item>
-                <Menu.SubMenu key="sub1" title="Location">
+                <Menu.SubMenu key="sub1" title="Location" icon={<EnvironmentOutlined />} >
                     <Menu.Item key="4">Quận 1</Menu.Item>
                     <Menu.Item key="5">Quận 2</Menu.Item>
                 </Menu.SubMenu>
--- src/modules/containers/User/index.js
+++ src/modules/containers/User/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Table, Input, Button } from 'antd';
+import { Table, Input, Button, Tooltip, Popconfirm, message } from 'antd';
 import {
-    PlusOutlined
+    PlusOutlined,
+    DeleteTwoTone,
+    EyeTwoTone,
+    EditTwoTone
 } from '@ant-design/icons';
-const { Column, ColumnGroup } = Table;
 import './index.css';
+
 const { Search } = Input;
+const { Column, ColumnGroup } = Table;
+
 const data = [
     {
         key: '1',
@@ -33,84 +38,119 @@ const data = [
     },
 ];
 
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        filters: [
-            {
-                text: 'Joe',
-                value: 'Joe',
-            },
-            {
-                text: 'Jim',
-                value: 'Jim',
-            },
-        ],
-        // specify the condition of filtering result
-        // here is that finding the name started with `value`
-        onFilter: (value, record) => record.name.indexOf(value) === 0,
-        sortDirections: ['descend'],
-    },
-    {
-        title: 'Phone',
-        dataIndex: 'phone',
-        defaultSortOrder: 'descend',
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        defaultSortOrder: 'descend',
-        sorter: (a, b) => a.age - b.age,
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        filters: [
-            {
-                text: 'London',
-                value: 'London',
-            },
-            {
-                text: 'New York',
-                value: 'New York',
-            },
-        ],
-        filterMultiple: false,
-        onFilter: (value, record) => record.address.indexOf(value) === 0,
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortDirections: ['descend', 'ascend'],
-    },
-    {
-        title: 'Action',
-        dataIndex: '',
-        key: 'x',
-        render: () => <a>Delete</a>,
-    }
-];
-
 
 const User = () => {
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            align: 'center',
+            filters: [
+                {
+                    text: 'Joe',
+                    value: 'Joe',
+                },
+                {
+                    text: 'Jim',
+                    value: 'Jim',
+                },
+            ],
+            // specify the condition of filtering result
+            // here is that finding the name started with `value`
+            onFilter: (value, record) => record.name.indexOf(value) === 0,
+            sortDirections: ['descend'],
+        },
+        {
+            title: 'Phone',
+            dataIndex: 'phone',
+            align: 'center',
+            defaultSortOrder: 'descend',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            align: 'center',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.age - b.age,
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            align: 'center',
+            filters: [
+                {
+                    text: 'London',
+                    value: 'London',
+                },
+                {
+                    text: 'New York',
+                    value: 'New York',
+                },
+            ],
+            filterMultiple: false,
+            onFilter: (value, record) => record.address.indexOf(value) === 0,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Action',
+            align: 'center',
+            dataIndex: '',
+            key: 'x',
+            render: () => (
+                <div className="user-action">
+                    <Tooltip placement="top" title={"Delete"}>
+                        <Popconfirm
+                            title="Bạn có chắc chắn muốn xóa?"
+                            onConfirm={confirm}
+                            onCancel={cancel}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <Button shape="circle" icon={<DeleteTwoTone twoToneColor="red" />} size="small" />
+                        </Popconfirm>
+                    </Tooltip>
+                    <Tooltip placement="top" title={"View"}>
+                        <Button shape="circle" icon={<EyeTwoTone twoToneColor="green" />} size="small" />
+                    </Tooltip>
+                    <Tooltip placement="top" title={"Edit"}>
+                        <Button shape="circle" icon={<EditTwoTone />} size="small" />
+                    </Tooltip>
+                </div>
+            ),
+        }
+    ];
     const onChangeTable = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
     }
     const onSearch = e => {
         console.log(e);
     };
+    const confirm = (e) => {
+        console.log(e);
+        message.success('Click on Yes');
+    }
+
+    const cancel = (e) => {
+        console.log(e);
+        message.error('Click on No');
+    }
     return (
         <div className="user-content">
             <div className="user-content-header">
                 <Search
-                    placeholder="input search text"
+                    placeholder="Tìm kiếm"
+                    maxLength={30}
+                    className="input-search-user"
                     allowClear
                     onSearch={onSearch}
-                    style={{ width: 200}}
+                    style={{ width: 200 }}
                 />
-                <Button type="primary" icon={<PlusOutlined />} size="medium">
+                <Button type="primary" size="medium">
                     Thêm mới
                 </Button>
             </div>
             <Table
+                bordered
                 dataSource={data}
                 columns={columns}
                 dataSource={data}
